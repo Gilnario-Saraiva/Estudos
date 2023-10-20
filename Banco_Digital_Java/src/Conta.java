06418f870cdb3b5b4b95d5422aa2ac9cd057e127
Gilnario Saraiva <gilnariosaraiva@gmail.com>
2023-10-19 21:54:50 -0300

    Exercício banco digital java

---
+++ Banco_Digital_Java/src/Conta.java
@@ -0,0 +1,43 @@
+
+public abstract class Conta implements InterfaceConta {
+	
+	private static final int AGENCIA_PADRAO = 1;
+	private static int SEQUENCIAL = 1;
+	
+	protected int agencia;
+	protected int numero;
+	protected double saldo;
+	
+	public Conta() {
+		this.agencia = Conta.AGENCIA_PADRAO;
+		this.numero = SEQUENCIAL++;
+	}
+	
+	@Override
+	public void sacar(double valor) {
+		
+	}
+
+	@Override
+	public void depositar(double valor) {
+		
+	}
+
+	@Override
+	public void transferr(double valor, Conta contaDestino) {
+		
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+}
