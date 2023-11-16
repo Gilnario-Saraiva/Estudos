f9a9099820f20582fe484183f22b72bb9f8c7a6e
Gilnario Saraiva <gilnariosaraiva@gmail.com>
2023-11-16 08:40:04 -0300

    Finalização do exercício de POO

--- Desafio_POO_Java_DIO/src/br/com/dio/desafio/dominio/Main.java
+++ Desafio_POO_Java_DIO/src/br/com/dio/desafio/dominio/Main.java
@@ -14,14 +14,36 @@ public class Main {
 		curso2.setDescricao("Curso de programação JavaScript");
 		curso2.setCargaHoraria(4);
 		
-		System.out.println(curso1);
-		System.out.println(curso2);
-		
 		Mentoria mentoria = new Mentoria();
 		mentoria.setTitulo("Java");
 		mentoria.setDescricao("Mentoria Java");
 		mentoria.setData(LocalDate.now());
 		
-		System.out.println(mentoria);
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Bootcamp Java Developer");
+		bootcamp.setDescricao("Bootcamp sobre Java POO");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria);
+		
+		Dev developer1 = new Dev();
+		developer1.setNome("Gilnario");
+		developer1.inscreverBootcamp(bootcamp);
+		System.out.println("Conteúdos inscritos " + developer1.getConteudosInscritos());
+		developer1.progredir();
+		System.out.println("Conteúdos inscritos " + developer1.getConteudosInscritos());
+		System.out.println("Conteúdos concluídos " + developer1.getConteudosConcluidos());
+		System.out.println("XP " + developer1.calcularTotalXp());
+		System.out.println("-------");
+		
+		Dev developer2 = new Dev();
+		developer2.setNome("Camila");
+		developer2.inscreverBootcamp(bootcamp);
+		developer2.progredir();
+		System.out.println("Conteúdos inscritos " + developer2.getConteudosInscritos());
+		developer2.progredir();
+		System.out.println("Conteúdos inscritos " + developer2.getConteudosInscritos());
+		System.out.println("Conteúdos concluídos " + developer2.getConteudosConcluidos());
+		System.out.println("XP " + developer2.calcularTotalXp());
 	}
 }
