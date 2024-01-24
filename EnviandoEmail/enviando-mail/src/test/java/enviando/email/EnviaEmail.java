d3203e6182011139add8a124f4f6e243ab4dcfb2
Gilnario Saraiva <gilnariosaraiva@gmail.com>
2024-01-23 22:15:42 -0300

    Enviando Email

---
+++ EnviandoEmail/enviando-mail/src/test/java/enviando/email/EnviaEmail.java
@@ -0,0 +1,65 @@
+package enviando.email;
+
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class EnviaEmail {
+
+	private String userName = "email@gmail.com";
+	private String password = "qqqqwwwweeeerrrr";
+	
+	private String listaDestinatarios = "";
+	private String nomeRemetente = "";
+	private String assuntoEmail = "";
+	private String textoEmail = "";
+	
+	public EnviaEmail(String listaDestinatarios, String nomeRemetente, String assuntoEmail, String textoEmail) {
+		this.listaDestinatarios = listaDestinatarios;
+		this.nomeRemetente = nomeRemetente;
+		this.assuntoEmail = assuntoEmail;
+		this.textoEmail = textoEmail;
+		
+	}
+
+	public void enviaEmail() {
+		try {
+
+			Properties properties = new Properties();
+			properties.put("mail.smtp.ssl.trust", "*");
+			properties.put("mail.smtp.auth", "true");
+			properties.put("mail.smtp.starttls", "true");
+			properties.put("mail.smtp.host", "smtp.gmail.com");
+			properties.put("mail.smtp.port", "465");
+			properties.put("mail.smtp.socketFactory.port", "465");
+			properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
+			Session session = Session.getInstance(properties, new Authenticator() {
+				@Override
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(userName, password);
+				}
+			});
+
+			Address[] tuUser = InternetAddress.parse(listaDestinatarios);
+			Message message = new MimeMessage(session);
+
+			message.setFrom(new InternetAddress(userName, nomeRemetente));
+			message.setRecipients(Message.RecipientType.TO, tuUser);
+			message.setSubject(assuntoEmail);
+			message.setText(textoEmail);
+
+			Transport.send(message);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
