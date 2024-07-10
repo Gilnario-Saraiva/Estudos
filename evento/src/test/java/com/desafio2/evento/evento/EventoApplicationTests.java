fb8dda81808e28c969d106c28693f3bdc6b4be5f
Gilnario Saraiva <gilnariosaraiva@gmail.com>
2024-07-09 22:06:54 -0300

    projeto DevSuperior

---
+++ evento/src/test/java/com/desafio2/evento/evento/EventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio2.evento.evento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
